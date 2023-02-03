c2c4793114cda6c1b56eb6b2ff4cc7d91b881c1c
xiaozhao <782717466@qq.com>
2023-02-03 00:16:15 -0800

    修改注释

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/ACking-you/byte_douyin_project/config"
 	"github.com/ACking-you/byte_douyin_project/router"
 )
 
 func main() {
+	// 使用大写保证其可见性
 	r := router.InitDouyinRouter()
 	err := r.Run(fmt.Sprintf(":%d", config.Info.Port)) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
